12ff55949e4caf3fd97d5aedec8d7c83289e664b
Lina C <linaclark96@gmail.com>
2017-10-16 06:10:27 -0400

    initial commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+    consumer_key: '5I295MwvmdmI9FUtcgWMDSXxn',
+    consumer_secret: '	Du2rF02Sn4D2EIQzxJj2Z0DeRWm3cv0NctFfxeCbKdM0y8SHLD',
+    access_token_key: '919857570260676608-tWRHnWwmvpGaOpn7RDcuKzxuTs2pgcf',
+    access_token_secret: '9G9sPAQ5FSULjV2y93QjMEHCnnxwU1XjgtuWWpcPzuVqc',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,20 @@
+//grab the data from keys.js
+
+//code for my-tweets
+//this will show your last 20 tweets
+
+
+//code for spotify-this-song
+//this will show the artist, song's name, preview link of the song and the album
+//if no song is provided, it will default to "The Sign" by Ace of Base
+
+//code for movie this
+//will output the title of the movie, release year, imdb rating, rotten tomatoes rating
+//country movie was produced, language of the movie, plot and actors
+//if no movie is provided, it will default to Mr.Nobody
+//use api key '40e9cece'
+
+//code for do-what-it-says
+//using fs, take the text inside the random text and use it to call one of Liri's commands
+
+//Bonus: output the data into a log.txt that you test from the commands
