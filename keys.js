3d0494a04c96685f9b744e447cb342f9a82aef66
Lina C <linaclark96@gmail.com>
2017-10-16 21:01:01 -0400

    spotify and twitter done

--- keys.js
+++ keys.js
@@ -1,10 +1,8 @@
 console.log('this is loaded');
 
-var twitterKeys = {
-    consumer_key: '5I295MwvmdmI9FUtcgWMDSXxn',
-    consumer_secret: '	Du2rF02Sn4D2EIQzxJj2Z0DeRWm3cv0NctFfxeCbKdM0y8SHLD',
+exports.twitterKeys = {
+    consumer_key: 'B50K9ZXuKrvVtjsfh5iJJspdz',
+    consumer_secret: '7yJ8LKDXPQnF2iGLgOLAohiAf6q7Lfz9Sk357znuzW87PX5C8s',
     access_token_key: '919857570260676608-tWRHnWwmvpGaOpn7RDcuKzxuTs2pgcf',
     access_token_secret: '9G9sPAQ5FSULjV2y93QjMEHCnnxwU1XjgtuWWpcPzuVqc',
-}
-
-module.exports = twitterKeys;
+};
--- liri.js
+++ liri.js
@@ -1,12 +1,83 @@
 //grab the data from keys.js
+var keys = require("./keys.js");
+var Twitter = require('twitter');
+var Spotify = require("node-spotify-api");
+//var request = require("request");
+var fs = require("fs");
+
+
+var pickArg = function(action, responseData) {
+    switch (action) {
+        case 'my-tweets':
+            myTweets();
+            break;
+        case 'spotify-this-song':
+            spotifyThatSong();
+            break;
+        case 'movie-this':
+            movieThis();
+            break;
+        case 'do-what-it-says':
+            doItWell();
+            break;
+        default:
+            console.log("I do not understan");
+    }
+}
+
+
 
 //code for my-tweets
 //this will show your last 20 tweets
+function myTweets() {
+
+    var twitterKeys = keys.twitterKeys;
+
+    var client = new Twitter(keys.twitterKeys);
+
+
+    var params = { screen_name: "linaclark96" };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log(tweets[i].text);
+                console.log("========================");
+            }
+        } else {
+            console.log(error);
+        }
+    });
+}
 
 
 //code for spotify-this-song
 //this will show the artist, song's name, preview link of the song and the album
 //if no song is provided, it will default to "The Sign" by Ace of Base
+function spotifyThatSong(name) {
+
+    var spotify = new Spotify({
+        id: "f2b90fa89d71401dbcee976d3aa148c4",
+        secret: "27f031d07a4f41f58bdae4dd7f99631f"
+    });
+
+    spotify.search({ type: 'track', query: name }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var track = data.tracks.items[0];
+        console.log(track);
+        console.log("=========================================");
+        console.log('artist: ' + track.artist);
+        console.log('song name: ' + track.name);
+        console.log('preview song: ' + track.preview_url);
+        console.log('album: ' + track.album.name);
+        console.log("=========================================");
+    });
+}
+
+spotifyThatSong("The Sign");
+
 
 //code for movie this
 //will output the title of the movie, release year, imdb rating, rotten tomatoes rating
